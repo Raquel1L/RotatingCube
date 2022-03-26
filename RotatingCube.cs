fe864ce7dfee62c62566dc7eba60bc3036943e36
F1lipe01 <102438221+F1lipe01@users.noreply.github.com>
2022-03-26 22:14:12 +0000

    Update RotatingCube.cs

--- RotatingCube.cs
+++ RotatingCube.cs
@@ -11,69 +11,68 @@ public class RotatingCube : MonoBehaviour
     private Text imageTrackedText;
 
     [SerializeField]
-    private GameObject[] arObjectsToPlace; // GameObjects "originais"
+    private GameObject[] arObjectsToPlace; // "original" GameObjects
     
     [SerializeField]
     private Vector3 scaleFactor = new Vector3(0.1f,0.1f,0.1f);
 
     private ARTrackedImageManager m_TrackedImageManager;
-    //dicionario com os GameObjects Instanciados (clones)
-    private Dictionary<string,GameObject> arObjects = new Dictionary<string, GameObject>(); // dicionario com a key = ao nome do objeto e o valor o GameObject instanciado
+    //dictionary with the closes
+    private Dictionary<string,GameObject> arObjects = new Dictionary<string, GameObject>(); // dictionary with the key = name of the object and value the GameObject
 
     void Awake()
         {
             m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
 
-            foreach(GameObject arObject in arObjectsToPlace){ // todos os GameObjects que foram adicionados pelo editor
-                GameObject newARObject = Instantiate(arObject,Vector3.zero,Quaternion.identity); // criação do objeto instanciado ( clone do original )
+            foreach(GameObject arObject in arObjectsToPlace){ // all the gameobjects added from the editor
+                GameObject newARObject = Instantiate(arObject,Vector3.zero,Quaternion.identity); // creation of the instanciated object
                 newARObject.SetActive(false);
-                newARObject.name = arObject.name; // adiciona-se o nome ao clone
-                arObjects.Add(arObject.name,newARObject); // guarda-se o clone no dicionario!
+                newARObject.name = arObject.name; // add the name to the clone
+                arObjects.Add(arObject.name,newARObject); // save the clone in the dictionary
             }
         
         }
 
         void OnEnable()
         {
-            m_TrackedImageManager.trackedImagesChanged += OnTrackedImagesChanged; // Subscreve o evento .trackedImagesChanges
-            // Sempre que uma trackedImage é alterada (adicionada,atualizada,removida) chama o metodo OnTrackedImagesChanged
-        }
+            m_TrackedImageManager.trackedImagesChanged += OnTrackedImagesChanged; // Subscribe the event o .trackedImagesChanges ( check EVENTS on Image Tracking)
+        }   // Every time a tracked Image is changed (added,updated,removed) it calls the method OnTrackedImagesChanged
 
         void OnDisable()
         {
-            m_TrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged; // Deixa de Subscrever ao evento
+            m_TrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged; // Unsubscribe the event
         }
 
         void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
         {
-            foreach (var trackedImage in eventArgs.added) // sempre que a imagem é detetada pela primeira vez
+            foreach (var trackedImage in eventArgs.added) // 1st detection of the image
                 UpdatePrefab(trackedImage);
-            foreach (var trackedImage in eventArgs.updated) // se continuar a ser mostrada
+            foreach (var trackedImage in eventArgs.updated) // everytime it keeps the detection of the image
                 UpdatePrefab(trackedImage);
-            foreach (var trackedImage in eventArgs.removed) // se for removida
-                arObjects[trackedImage.name].SetActive(false); // retira o prefab se a imagem desaparecer
+            foreach (var trackedImage in eventArgs.removed) // if its been removed
+                arObjects[trackedImage.name].SetActive(false); // the prefab ( cube ) disappear 
             
         }
-        void UpdatePrefab(ARTrackedImage trackedImage) // trackedImage.referenceImage = Imagem de referencia procurada no cenario ( mundo real )
+        void UpdatePrefab(ARTrackedImage trackedImage) // trackedImage.referenceImage = reference image to "find" in the real world
         {  
-            imageTrackedText.text = trackedImage.referenceImage.name; // Permite guardar o nome da imagem que foi detetada em questão (QRCODE)
-                                                                      // Para depois mostrar na aplicação o nome da imagem (QRCODE)
+            imageTrackedText.text = trackedImage.referenceImage.name; // Keep the name of the image
+                                                                      // Useful if you want to print it like me on the TEXT component
 
-            AssignGameObject(trackedImage.referenceImage.name,trackedImage.transform.position); // posicionar e "ativar" (mostrar no mundo) os clones
+            AssignGameObject(trackedImage.referenceImage.name,trackedImage.transform.position); // establish the position and activates the "clone" ( show im in the world)
         
             Debug.Log($"trackedImage.referenceImane.name: {trackedImage.referenceImage.name}");
         }
 
         void AssignGameObject(string name, Vector3 newPosition){
-            // o parametro nome permite alterar o colone desejado pois o nome corresponde à chave do dicionario (identificador dos elementos)
-            if(arObjectsToPlace != null){ // se forem adicionados os GameObjects no editor 
-                arObjects[name].SetActive(true); // fica visivel
-                arObjects[name].transform.position = newPosition; // vai para a posição newPosition ( neste caso corresponde à propria posicao da imagem)
+            // The name parameter is important because it allows the code to identify the proper clone we want to add ( the key of the dictionary is this string - name)
+            if(arObjectsToPlace != null){ // if you dont forget to fullfull the list of game objects
+                arObjects[name].SetActive(true); // its visible
+                arObjects[name].transform.position = newPosition; // go to a position (in this case = to the image position)
                 arObjects[name].transform.Rotate(5,0,0);
-                arObjects[name].transform.localScale = scaleFactor; // fica com a escala scaleFactor (valor normal - retirado da UNITY - dimensao pequena e visivel)
+                arObjects[name].transform.localScale = scaleFactor; // scale (default value 10cm(x,y,z))
                 foreach(GameObject go in arObjects.Values){ 
                     Debug.Log($"Go in arObjects.Values: {go.name}");
-                    if(go.name != name){ // coloca todos inativos menos o atual -> evita sobreposição de imagens
+                    if(go.name != name){ // All images go disappear except the right one!
                         go.SetActive(false);
                     }
                 }
